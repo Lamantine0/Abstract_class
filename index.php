bf38aff653f40ded899fecd9385cf9b5bd6dd041
dimatereshko95 <dimatereshko95@gmail.com>
2021-01-29 12:19:02 +0300

    Создание абстрактного класса. Наследнование от него. Создание метода который проводит простейшие вычесление.

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+
+abstract class  TestClass{
+
+    public $a;
+    public $b;
+
+    function __construct($a , $b)
+    {
+        $this->a=$a;
+        $this->b=$b;
+
+
+    }
+
+    abstract public function multiplication();
+
+}
+
+
+class  TextMessege extends  TestClass{
+
+    public function multiplication()
+    {
+        return $this->a*$this->b;
+    }
+}
+
+$decision = new TextMessege(2,2);
+echo $decision->multiplication();
